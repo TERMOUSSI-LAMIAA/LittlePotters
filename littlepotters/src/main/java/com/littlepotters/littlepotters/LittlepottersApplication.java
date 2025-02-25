6101b2c71781bbd060c9666f6414fd8a85fe47e9
TERMOUSSI-LAMIAA <lamiaa3105@gmail.com>
2025-02-25 20:47:32 +0000

    structure

---
+++ littlepotters/src/main/java/com/littlepotters/littlepotters/LittlepottersApplication.java
@@ -0,0 +1,13 @@
+package com.littlepotters.littlepotters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittlepottersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittlepottersApplication.class, args);
+	}
+
+}
---
+++ littlepotters/src/test/java/com/littlepotters/littlepotters/LittlepottersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.littlepotters.littlepotters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittlepottersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
