a27083e392146d24d35c99000553198d75fe137f
TERMOUSSI-LAMIAA <lamiaa3105@gmail.com>
2025-02-26 10:20:32 +0000

    LIT-1 User & Role entities

---
+++ backend/littlepotters/src/main/java/com/littlepotters/littlepotters/models/Role.java
@@ -0,0 +1,20 @@
+package com.littlepotters.littlepotters.models;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @NonNull
+    private String name;
+}
---
+++ backend/littlepotters/src/main/java/com/littlepotters/littlepotters/models/User.java
@@ -0,0 +1,46 @@
+package com.littlepotters.littlepotters.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String fullname;
+
+    @NotBlank
+    private String phone;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+}
