c769346383348cf518858273c642e9f64f1c65af
TERMOUSSI-LAMIAA <lamiaa3105@gmail.com>
2025-02-26 12:13:50 +0000

    LIT-1 User& role repositories

---
+++ backend/littlepotters/src/main/java/com/littlepotters/littlepotters/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.littlepotters.littlepotters.repositories;
+
+import com.littlepotters.littlepotters.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+}
---
+++ backend/littlepotters/src/main/java/com/littlepotters/littlepotters/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.littlepotters.littlepotters.repositories;
+
+import com.littlepotters.littlepotters.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
