9fcae21bee6104a6cf6efba03eb7f42802e0d1d9
TERMOUSSI-LAMIAA <lamiaa3105@gmail.com>
2025-03-12 00:01:42 +0000

    LIT-45 auth guard in routes

--- frontend/littlePotters/src/app/app.routes.ts
+++ frontend/littlePotters/src/app/app.routes.ts
@@ -5,13 +5,26 @@ import { RegisterComponent } from './features/auth/components/register/register.
 import { InstructorDashboardComponent } from './instructor-dashboard/instructor-dashboard.component';
 import { CustomerSpaceComponent } from './customer-space/customer-space.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AuthGuard } from './core/auth/guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'admin-dashboard', component: AdminDashboardComponent },
-    { path: 'instructor-dashboard', component: InstructorDashboardComponent },
-    { path: 'customer-space', component: CustomerSpaceComponent },
+    {
+        path: 'admin-dashboard', component: AdminDashboardComponent,
+        canActivate: [AuthGuard],
+        data: { role: 'ROLE_ADMIN' }
+    },
+    {
+        path: 'instructor-dashboard', component: InstructorDashboardComponent,
+        canActivate: [AuthGuard],
+        data: { role: 'ROLE_INSTRUCTOR' }
+    },
+    {
+        path: 'customer-space', component: CustomerSpaceComponent,
+        canActivate: [AuthGuard],
+        data: { role: 'ROLE_CUSTOMER' }
+    },
 ];
